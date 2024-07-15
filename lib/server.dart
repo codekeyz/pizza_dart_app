3d3ee712f06cab11ce4fff629f752e945a892409
Chima Precious <ositachima.co@gmail.com>
2024-07-15 09:35:22 +0000

    _

--- lib/server.dart
+++ lib/server.dart
@@ -12,7 +12,7 @@ void main() async {
   app.get(
     '/',
     (req) => Response.ok(
-      "Heya pizza 🍕",
+      "Heya 🍕",
       headers: {HttpHeaders.contentTypeHeader: 'text/plain; charset=utf-8'},
     ),
   );
